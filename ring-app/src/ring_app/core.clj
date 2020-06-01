0d8cd2ba289553b49da883dec12bfb226656b6e1
Phil Hunt <phil@deviantlogic.co.uk>
2020-06-01 16:33:28 +0100

    routing with Reitit

--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -1,5 +1,6 @@
 (ns ring-app.core
-  (:require [muuntaja.middleware :as muuntaja]
+  (:require [reitit.ring :as reitit]
+            [muuntaja.middleware :as muuntaja]
             [ring.adapter.jetty :as jetty]
             [ring.util.http-response :as response]
             [clojure.pprint :as pp]
@@ -10,14 +11,44 @@
   (response/ok
    {:result (get-in request [:body-params :id])}))
 
-(defn http-handler [request-map]
+(defn response-handler [request-map]
   "Simple handler. Returns caller's IP"
   (response/ok
    (str "<html><body> Hello Ana, your IP is "
-        (:remote-addr request-map)()
+        (:remote-addr request-map)
         "</body></html>")))
 
-(def handler json-handler)
+(defn wrap-formats [handler]
+  (-> handler
+      (muuntaja/wrap-format)))
+
+(def routes
+  [["/" {:get response-handler
+         :post response-handler}]
+   ["/echo/:id"
+    {:get
+     (fn [{{:keys [id]}  :path-params}]
+       (response/ok (str "<p> the value is: " id "</p")))}]
+   ["/api"
+    {:middleware [wrap-formats]} ; this lets us use wrap-format only for api
+    ["/multiply"
+     {:post
+      (fn [{{:keys [a b]} :body-params}] ; see
+        (response/ok {:result (* a b)}))}]]])
+
+(def handler
+  (reitit/routes
+   (reitit/ring-handler
+    (reitit/router routes))
+   (reitit/create-resource-handler
+    {:path "/"})
+   (reitit/create-default-handler
+    {:not-found
+     (constantly (response/not-found "404 - Page not found motherfucker"))
+     :method-not-allowed
+     (constantly (response/method-not-allowed "405 - Not Allowed"))
+     :not-acceptable
+     (constantly (response/not-acceptable "406 - Not acceptable"))})))
 
 (defn wrap-nocache [http-handler]
   "uses assoc-in to add a new header and header value to response-map"
@@ -26,15 +57,11 @@
         handler ; function passed in, returns a response map
         (assoc-in [:headers "Pragma"] "no-cache")))) ; adds header to map
 
-(defn wrap-formats [handler]
-  (-> handler
-      (muuntaja/wrap-format)))
 
 (defn -main []
   (jetty/run-jetty
    (-> #'handler ; pass a reference to the handler, don't eval it yet
        wrap-nocache ; threader macro passes handler to wrapper
-       wrap-formats
-       wrap-reload) ; FIXME does handler get re-evaluated in here?
+       wrap-reload) ; does handler get re-evaluated in here?
    {:port 3000
     :join? false}))
